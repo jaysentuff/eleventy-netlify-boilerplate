83f9d251da93fc70c09f93d5c27cc7f6359f56b1
jaysentuff <87148810+jaysentuff@users.noreply.github.com>
2025-08-05 10:11:47 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -75,6 +75,7 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy({ "static/css": "css" });
   eleventyConfig.addPassthroughCopy("admin/");
   eleventyConfig.addPassthroughCopy("_includes/assets/css/inline.css");
 
